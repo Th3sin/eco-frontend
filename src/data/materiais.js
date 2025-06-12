9545e0b21ee1615363956ea56850940c82f4813d
Thesin <rm95565@estudante.fieb.edu.br>
2025-06-12 18:42:36 -0300

    correcao listas

--- src/data/materiais.js
+++ src/data/materiais.js
@@ -5,9 +5,8 @@ export const tipoResiduo = [
   "Perfurocortante", "Químico", "Radioativo", "Plástico", "Vidro", 
   "Metais", "Papel", "Agroindustrial", "Inerte", "Tóxico", "Inflamável", 
   "Vegetal", "Pilhas e Baterias", "Medicamentos", "Madeira", 
-  "Borracha", "Alumínio", "Isopor", "Óleo de Cozinha", 
-  "Resíduos de Construção Civil", "Resíduos Industriais", 
-  "Resíduos de Serviços de Saúde"
+  "Borracha", "Alumínio", "Isopor", "Óleos", 
+  "Resíduos de Construção Civil", 
 ];
 
 export const materiais = {
